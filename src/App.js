306f0df0a86b5a6a3432090730c42af97fcca822
Elena-277 <129676247+Elena-277@users.noreply.github.com>
2023-09-10 11:05:10 -0400

    lesson 1-1

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
 
+const todoList = [
+  {
+    id: 1,
+    title: 'do_no_1'
+  },
+  {
+    id: 2,
+    title: 'do_no_2'
+  },
+  {
+    id: 3,
+    title: 'do_no_3'
+  }
+]
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Todo list</h1>
+      <ul>
+        {
+          todoList.map(function(item){
+            return <li key={item.id}>{item.title}</li>;
+          }
+          )
+        }
+      </ul>
     </div>
   );
 }
