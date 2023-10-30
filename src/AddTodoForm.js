7c1a3780c2862e1d9b2d94bafd05e06ef51f17be
Elena-277 <129676247+Elena-277@users.noreply.github.com>
2023-10-30 16:43:58 -0400

    lesson-1-4

--- src/AddTodoForm.js
+++ src/AddTodoForm.js
@@ -1,20 +1,27 @@
 import React from 'react';
 
-function AddTodoForm(props) {
-    function handleAddTodo(event){
+function AddTodoForm({onAddTodo}) {    
+    const [todoTitle, setTodoTitle] = React.useState('');
+    function handleTitleChange(event) {
         event.preventDefault();
-        let todoTitle = event.target.title.value;
-        console.log(todoTitle);
-        props.onAddTodo(todoTitle);
-        event.target.reset();
+        const newTodoTitle = event.target.value;
+        setTodoTitle(newTodoTitle);
+    }
+    function handleAddTodo(event) {
+        event.preventDefault();
+        onAddTodo({            
+            id: Date.now(),
+            title: todoTitle
+        });
+        setTodoTitle('');
     }
     return (
         <form onSubmit={handleAddTodo}>
             <label htmlFor="todoTitle">Title </label>
-            <input id="todoTitle" name="title"></input>
+            <input id="todoTitle" name="title" value={todoTitle} onChange={handleTitleChange}></input>
             <button>Add</button>
         </form>
     );
-  }
-  
-  export default AddTodoForm;
+}
+
+export default AddTodoForm;
--- src/TodoList.js
+++ src/TodoList.js
@@ -1,27 +1,12 @@
 import React from 'react';
 import TodoListItem from './TodoListItem'; 
 
-const todoList = [
-    {
-      id: 1,
-      title: 'do_no_1'
-    },
-    {
-      id: 2,
-      title: 'do_no_2'
-    },
-    {
-      id: 3,
-      title: 'do_no_3'
-    }
-  ]
-
-function TodoList() {
+function TodoList({todoList}) {
     return (
         <ul>
           {
-            todoList.map(function(item){
-              return <TodoListItem key={item.id} todo ={item} />;
+            todoList.map(function(item){              
+              return <TodoListItem key={item.id} todo={item} />;
             }
             )
           }
